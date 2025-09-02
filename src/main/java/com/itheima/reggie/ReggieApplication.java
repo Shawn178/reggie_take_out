c6cf10d1341721d826bb42afbbe808b16e58935c
Java_Project <Java@ymh.cn>
2025-09-02 19:36:56 +0800

    使用Sharding-JDBC实现程序的读写分离

--- src/main/java/com/itheima/reggie/ReggieApplication.java
+++ src/main/java/com/itheima/reggie/ReggieApplication.java
@@ -1,19 +1,23 @@
 package com.itheima.reggie;
 
 import lombok.extern.slf4j.Slf4j;
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 // @slf4j 引入日志
 @Slf4j
-@SpringBootApplication
 @ServletComponentScan // 扫描过滤器
 // @EnableTransactionManagement 开启事务管理
 @EnableTransactionManagement
 @EnableCaching  // 开启Spring Cache注解方式的缓存功能
+@MapperScan("com.itheima.reggie.mapper")
+// 禁用数据源自动配置
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class ReggieApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/itheima/reggie/config/MyBatisPlusConfig.java
+++ src/main/java/com/itheima/reggie/config/MyBatisPlusConfig.java
@@ -1,7 +1,6 @@
 package com.itheima.reggie.config;
 
 import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
-import com.baomidou.mybatisplus.extension.plugins.PaginationInterceptor;
 import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -11,7 +10,7 @@ import org.springframework.context.annotation.Configuration;
 public class MyBatisPlusConfig {
 
     @Bean
-    // 创建MyBatisPlus的分页插件。这是 MyBatis-Plus 3.5+ 的“插件容器”，用来装多个“内部拦截器”(InnerInterceptor)。
+    // 创建MyBatisPlus的分页插件。这是 MyBatis-Plus 3.5+ 的"插件容器"，用来装多个"内部拦截器"(InnerInterceptor)。
     public MybatisPlusInterceptor mybatisPlusInterceptor(){
         // 创建拦截器容器实例
         MybatisPlusInterceptor mybatisPlusInterceptor = new MybatisPlusInterceptor();
